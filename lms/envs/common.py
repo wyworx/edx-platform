ef7028022bffd702b05fb5c80a462f59a7492153
David Ormsbee <dormsbee@edx.org>
2020-03-05 09:41:49 -0500

    Create learning_sequences app, Course Outline API.
    Introduces the learning_sequences app, intended to provide metadata for
    sequences and course outlines. The short term goal is to provide faster
    source of this information for the new Courseware microfrontend
    (frontend-app-learning). The medium term goal is to provide an in-proc
    API that is useful to other parts of the platform that need fast access
    to course outline information customized for a user. The long term
    goals are outlined in the README.rst.
    
    This first iteration of the API only lays out the basic structure for
    how we'd arrange the pieces, with enough of an implementation to feel
    "real" (simple schedules, staff_only content hiding). It's not ready to
    be turned on, and is not currently plugged into the publish-flow. The
    only way to get data into this new API is via the update_course_outline
    management command. The REST endpoint is also currently limited to
    global staff only, though it's possible to get the outline for a student
    by using the ?username= query parameter.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2562,6 +2562,9 @@ INSTALLED_APPS = [
     # Management of per-user schedules
     'openedx.core.djangoapps.schedules',
     'rest_framework_jwt',
+
+    # Learning Sequence Navigation
+    'openedx.core.djangoapps.content.learning_sequences.apps.LearningSequencesConfig',
 ]
 
 ######################### CSRF #########################################
